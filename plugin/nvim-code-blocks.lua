5ad0afe5e89d0388441c98b5caaf7f0eb6864d52
Justin Sonntag <justin.sonntag@gmail.com>
2025-11-18 15:52:19 -0800

    Fix spacing

--- plugin/nvim-code-blocks.lua
+++ plugin/nvim-code-blocks.lua
@@ -1,20 +1,20 @@
 -- Prevent loading plugin twice
 if vim.g.loaded_nvim_code_blocks == 1 then
-	return
+    return
 end
 vim.g.loaded_nvim_code_blocks = 1
 
 -- Create user commands
 vim.api.nvim_create_user_command("CodeBlockYank", function(opts)
-	local register = opts.args ~= "" and opts.args or nil
-	require("nvim-code-blocks").yank_block(register)
+    local register = opts.args ~= "" and opts.args or nil
+    require("nvim-code-blocks").yank_block(register)
 end, { nargs = "?", desc = "Yank code block to register" })
 
 vim.api.nvim_create_user_command("CodeBlockDelete", function(opts)
-	local register = opts.args ~= "" and opts.args or nil
-	require("nvim-code-blocks").delete_block(register)
+    local register = opts.args ~= "" and opts.args or nil
+    require("nvim-code-blocks").delete_block(register)
 end, { nargs = "?", desc = "Delete code block to register" })
 
 vim.api.nvim_create_user_command("CodeBlockHighlight", function()
-	require("nvim-code-blocks").highlight_block()
+    require("nvim-code-blocks").highlight_block()
 end, { desc = "Highlight code block" })
